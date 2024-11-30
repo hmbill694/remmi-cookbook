fe73cd4a8ff4d0827148e32b3fb1ea1f1bc7f3fc
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2024-11-29 23:46:05 -0700

    formatted

--- cmd/model/recipe.go
+++ cmd/model/recipe.go
@@ -2,12 +2,11 @@ package model
 
 import "time"
 
-
 type Recipe struct {
-	Id string `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"author"`
-	Description string `json:"description"`
-	CreatedAt time.Time `json:"createdAt"`
-	EditedAt time.Time `json:"editedAt"`
+	Id          string    `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdAt"`
+	EditedAt    time.Time `json:"editedAt"`
 }
